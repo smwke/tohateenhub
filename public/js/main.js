839c369029740bb902a173fcda591d8ac7877f77
Dorin <dorincascaval@gmail.com>
2019-05-24 18:25:45 +0300

    Done cleanup of front-end files

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,18 @@
-let navbar = document.getElementById("scrolledNavbar");
+$(window).on("load", () => {
+  $('.preloader-wrapper').fadeOut();
+  $('body').removeClass('preloader-site');
+});
 
-if(typeof lockedNavbar == 'undefined'){
-  console.log("am intrat");
-  window.onscroll = myFunction();
-  $("#scrolledNavbar").toggleClass("scrolled-nav",(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40));
-}
-
-function myFunction() {
-  console.log("scrolling");
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    $("#scrolledNavbar").toggleClass("scrolled-nav",true);
-  } else{
-    $("#scrolledNavbar").toggleClass("scrolled-nav",false);
-  }
+if (typeof lockedNavbar == 'undefined') {
+  $(window).on("scroll", () => {
+      if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+          $("#scrolledNavbar").toggleClass("scrolled-nav", true);
+      } else {
+          $("#scrolledNavbar").toggleClass("scrolled-nav", false);
+      }
+  })
+  $("#scrolledNavbar").toggleClass("scrolled-nav", (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40));
+}else{
+  $("#bodyWrapper").toggleClass("scrolled",true);
+  $("#scrolledNavbar").toggleClass("scrolled-nav", true);
 }
