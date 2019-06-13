1ce96592cc5fc14dae150b007ff5bb2bbb115ac8
Dorin <dorincascaval@gmail.com>
2019-06-13 23:00:59 +0300

    added some news

--- app.js
+++ app.js
@@ -678,14 +678,14 @@ app.post("/send-message", (req, res) => {
     })
 });
 
-var httpServer = http.createServer(app);
-//var httpsServer = https.createServer(credentials,app);
+//var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials,app);
 
 //httpServer.listen(port);
-//httpsServer.listen(port);
+httpsServer.listen(port);
 
 
-// Start server
+/* Start server
 httpServer.listen(port, () => {
     console.log("Server started on: " + port);
-});
+});*/
